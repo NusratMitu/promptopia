c0a7f69bd3c70996ee752f2ef89c0fb34baaf745
NusratMitu <nusratmitu010@.com>
2023-08-09 16:57:51 +0600

    set up

---
+++ app/page.jsx
@@ -0,0 +1,14 @@
+
+const Home = () => {
+  return (
+    <section className="w-full flex-center flex-col">
+        
+    <h1 className="head_text text_center">
+        Discover & Share
+
+    </h1>
+    </section>
+  )
+}
+
+export default Home
