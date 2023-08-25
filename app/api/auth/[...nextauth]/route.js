7fdb239d6623e0001cf4bce3934d44ee6302ba77
NusratMitu <nusratmitu010@gmail.com>
2023-08-26 00:03:59 +0600

    db connect

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,9 +1,10 @@
 import NextAuth from "next-auth";
 import GooglePRovider from 'next-auth/providers/google';
-
+// import { connectToDB } from "@utils/database";
 
 console.log({ clientId: process.env.GOOGLE_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET});
+    
 const handler = NextAuth({
     providers: [GooglePRovider({
         clientId: process.env.GOOGLE_ID,
@@ -19,9 +20,9 @@ try {
     // 
 }
 cache (error) {
-
+console.log(error);
 }
-})
+}})
 
 export { handler as GET, handler as POST };
 
