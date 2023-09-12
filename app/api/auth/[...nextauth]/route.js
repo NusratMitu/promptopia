79d4531d88584fa4c316f49a535927a5bdb0ac82
NusratMitu <nusratmitu010@gmail.com>
2023-09-12 21:46:27 +0600

    session user

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,8 @@
+import User from "@models/user";
 import { connectToDB } from "@utils/database";
 import NextAuth from "next-auth";
 import GooglePRovider from 'next-auth/providers/google';
+
 console.log({ clientId: process.env.GOOGLE_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET});
     
@@ -11,16 +13,29 @@ const handler = NextAuth({
     })
 ],
 async session({session}) {
-
+    const sessionUser = await User.findOne({
+        email: session.user.email // get the current email from the session
+    })
+    session.user.id = sessionUser._id.toString()
+    return session
 },
 
 async signIn({profile}) {
 try {
-  await connectToDB()
+  await connectToDB();
     // check if a user is already exist
-    // cosnt User = 
+    const UserExists = await User.findOne({
+        email: profile.email
+    })
 
     //if not create a new user
+    if(!UserExists) {
+        await User.create({
+            email: profile.email, 
+            username: profile.name.replace(" ", "").toLowerCase(),
+            image: profile.picture
+        })
+    }
     return true;
 }
 cache (error) {
